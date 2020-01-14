6e3de2644687a9f2f3d4835e2e30262e171494e6
Igor Gomonov <i.gomonov@melsoft-games.com>
2020-01-14 16:17:08 +0300

    add lfu cache realization

---
+++ lfu/lfu.go
@@ -0,0 +1,150 @@
+package lfu
+
+import (
+	"container/list"
+	"sync"
+)
+
+type cache struct {
+	lock                sync.Mutex
+	items               map[string]*list.Element
+	frequencyItems      map[uint]*list.List
+	startFrequency      uint
+	currentMinFrequency uint
+	capacity            uint
+	size                uint
+}
+
+func NewCache(capacity uint) *cache {
+	return &cache{
+		items:               make(map[string]*list.Element),
+		frequencyItems:      make(map[uint]*list.List),
+		capacity:            capacity,
+		startFrequency:      1,
+		currentMinFrequency: 1,
+	}
+}
+
+type entry struct {
+	key       string
+	value     interface{}
+	frequency uint
+}
+
+func (c *cache) Add(key string, value interface{}) {
+	c.lock.Lock()
+	c.add(key, value)
+	c.lock.Unlock()
+}
+
+func (c *cache) Get(key string) (value interface{}) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+
+	return c.get(key)
+}
+
+func (c *cache) add(key string, value interface{}) {
+	if c.size >= c.capacity {
+		c.evict()
+	}
+
+	if item, ok := c.items[key]; ok {
+		entry := item.Value.(*entry)
+		oldFreq := entry.frequency
+
+		entry.value = value
+		entry.frequency++
+
+		if c.frequencyItems[entry.frequency] == nil {
+			c.frequencyItems[entry.frequency] = list.New()
+		}
+		c.frequencyItems[entry.frequency].PushFront(item)
+
+		li := c.frequencyItems[oldFreq]
+		li.Remove(item)
+		c.size++
+		return
+	}
+
+	if c.frequencyItems[c.startFrequency] == nil {
+		c.frequencyItems[c.startFrequency] = list.New()
+	}
+
+	elm := c.frequencyItems[1].PushFront(&entry{key: key, value: value, frequency: 1})
+	c.items[key] = elm
+	c.size++
+}
+
+func (c *cache) addd(key string, value interface{}) {
+	if c.size > c.capacity {
+		c.evict()
+	}
+
+	if item, ok := c.items[key]; ok {
+		entry := item.Value.(*entry)
+		oldList := c.frequencyItems[entry.frequency]
+		oldList.Remove(item)
+
+		entry.value = value
+		entry.frequency++
+
+		if c.frequencyItems[entry.frequency] == nil {
+			c.frequencyItems[entry.frequency] = list.New()
+		}
+
+		c.frequencyItems[entry.frequency].PushFront(item)
+		c.size++
+		return
+	}
+
+	if c.frequencyItems[c.startFrequency] == nil {
+		c.frequencyItems[c.startFrequency] = list.New()
+	}
+
+}
+
+func (c *cache) get(key string) interface{} {
+	if item, ok := c.items[key]; ok {
+		entry := item.Value.(*entry)
+		entry.frequency++
+
+		if c.frequencyItems[entry.frequency] == nil {
+			c.frequencyItems[entry.frequency] = list.New()
+		}
+
+		c.frequencyItems[entry.frequency].PushFront(item)
+		return entry.value
+	}
+	return nil
+}
+
+func (c *cache) evict() {
+	li := c.frequencyItems[c.currentMinFrequency]
+	if li != nil && li.Len() != 0 {
+		c.delete(li)
+		return
+	}
+	c.currentMinFrequency++
+	c.evict()
+}
+
+func (c *cache) delete(li *list.List) {
+	var elm *list.Element
+
+	if li != nil {
+		elm = li.Back()
+		ent := elm.Value.(*entry)
+
+		if li.Len() > 1 {
+			delete(c.items, ent.key)
+			li.Remove(elm)
+			c.size--
+			return
+		}
+
+		c.frequencyItems[c.currentMinFrequency] = nil
+		delete(c.items, ent.key)
+		c.size--
+	}
+}
---
+++ lfu/lfu_test.go
@@ -0,0 +1,22 @@
+package lfu
+
+import "testing"
+
+func TestLFU(t *testing.T) {
+	cache := NewCache(5)
+
+	cache.Add("key", 10)
+	cache.Add("key2", 11)
+	cache.Add("key2", 12)
+	cache.Add("key4", 9)
+	cache.Add("key5", 13)
+	cache.Add("key6", 15)
+
+	actValue := cache.Get("key")
+	t.Log(actValue)
+
+	for i, v := range cache.frequencyItems {
+		t.Log(i)
+		t.Log(v.Len())
+	}
+}
